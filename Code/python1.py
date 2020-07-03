197236ab1969569d29c6ee9fdb2bbb6dd20a4a22
coolwill92 <willpearce101@gmail.com>
2020-07-03 13:09:06 +0100

    Book: Question Three

--- Code/python1.py
+++ Code/python1.py
@@ -2,6 +2,12 @@
 
 #    return ("Hello World")
 
-def helloworld2():
-    x = "Hello World"
+#def helloworld2():
+#    x = "Hello World"
+#    return x
+
+def helloworld3():
+    x = input("Please enter a word:")
     return x
+
+
--- Tests/assessment_test.py
+++ Tests/assessment_test.py
@@ -5,5 +5,11 @@ from Code import python1
 # def test_one():
 #    assert python1.helloworld()== "Hello World"
 
-def test_two():
-    assert python1.helloworld2() == "Hello World"
+#def test_two():
+#    assert python1.helloworld2() == "Hello World"
+
+def test_three():
+    assert python1.helloworld3() == "HelloWorld"
+    assert python1.helloworld3() == "Williamiscool"
+    assert python1.helloworld3() == " "
+
